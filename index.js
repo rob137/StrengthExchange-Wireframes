ecc29f7e52d527b7454ab1437d61c415e651f6e7
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-12 15:05:10 +0000

    add edit workout page, fix carousel

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 let count = 0
-$('.next-wireframe-button').on('click', () => {
+
+function rotateWireframe() {
 	let nextWireframeHtml;
-	count++
+	
+	if (count === 0) {
+		nextWireframeHtml = `
+
+	<section class="hero landing-page-section">
+			<h1>StrengthExchange</h1>
+		</section>
+		<section class="description landing-page-section">
+			<p class="description-para">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</section>
+		<section class="login-signup landing-page-section">
+			<h2>Sign Up</h2>
+			<p>Already registered? Click <a href="blank">here</a> to sign in</p>
+			<form>
+				<label for="name">Name</label><br>
+				<input id="name" type="text" name=""><br>
+				<label for="email">Email</label><br>
+				<input id="email" type="text" name=""><br>
+				<label for="password">Password</label><br>
+				<input id="password" type="text" name=""><br>
+				<label for="confirm-password">Confirm password</label><br>
+				<input id="confirm-password" type="text" name=""><br><br>
+				<input type="submit" name="sign up" class="submit-button">
+			</form>
+		</section>
+	<footer><p>Footer</p><footer/>
+
+		`
+	}
 
 	// DASHBOARD
-	if (count === 1) {
+	else if (count === 1) {
 		nextWireframeHtml = `
 
 
@@ -40,7 +76,7 @@ $('.next-wireframe-button').on('click', () => {
 				</ul>
 			</main>
 		</section>
-
+		<footer><span>Footer</span><footer/>
 
 		`;
 
@@ -118,12 +154,41 @@ $('.next-wireframe-button').on('click', () => {
 				<li class="calendar day">
 					<span class="li-text">Day 5</span>
 				</li>
-
+				<footer><span>Footer</span><footer/>
 `;
 
 	// EDIT INDIVIDUAL WORKOUT
 	} else if (count ===3) {
-		nextWireframeHtml = ``; 
+		nextWireframeHtml = `
+			<nav>
+		<div class="burger-icon">[Menu]</div>
+		<h3>StrengthExchange</h3>
+	</nav>
+	<header class="edit-workout-header">
+		<h2>Joe Bloggs' Beach Workout - Day 5</h2>
+	</header>
+	<main class="edit-individual-workout">
+		<form>
+			<h2>Edit workout</h2>
+			<label>Exercise</label><br>
+			<input type="text" name=""><br>
+			<label>Weight</label><br>
+			<input type="number" name=""><br>
+			<label>Reps</label><br>
+			<input type="number" name=""><br>
+			<label>Sets</label><br>
+			<input type="number" name=""><br>
+			<label>Time</label><br>
+			<input type="time" name=""><br>
+			<label>Notes</label><br>
+			<input type="text" name=""><br><br>
+			<input type="submit" value="Add To Workout" name="Add">
+			<button>Cancel</button>
+		</form>
+	</main>
+	<footer><span>Footer</span><footer/>
+
+		`; 
 	
 	// LANDING PAGE (index.html)
 	} else {
@@ -160,12 +225,17 @@ $('.next-wireframe-button').on('click', () => {
 			</form>
 		</section>
 	</div>
-
+	<footer><span>Footer</span><footer/>
 
 
 
 	`;
 		count = 0;
 	}
-	$('.wireframe-content').html(nextWireframeHtml)
-});
+	console.log(count);
+	$('.wireframe-content').html(nextWireframeHtml);
+	count++
+}
+
+$(rotateWireframe)
+$('.next-wireframe-button').on('click', rotateWireframe);
